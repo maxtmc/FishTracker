e29daa7fee9ebfe3b8653d3738176ee472f98943
Max McShane <mm709020@southeast.edu>
2021-04-25 10:51:36 -0500

    Add project files.

---
+++ FishTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FishTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
